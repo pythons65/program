327a96d9868bab06f26f9f0e72aa3843fba3c76e
rabinlc <42234876+rabinlc@users.noreply.github.com>
2019-08-15 12:22:14 +0545

    Update program.py

--- program.py
+++ program.py
@@ -1,1 +1,21 @@
+import index as id
+import time as t
+e = False
+while e == False: 
+  code = input("PY_RT > ")
+  id.stru(code)
+  array = code.split()
+  if array[0] == code:
+    pass  
+  else:
+    b = 0 # 4
 
+    for i in range(0,len(array),1):
+      try:
+        a = float(array[i])# 5
+    
+        b = b + a# b = 4 + 5 
+      except:
+        a = array[i]   
+    print(b)   
+  t.sleep(1)  
